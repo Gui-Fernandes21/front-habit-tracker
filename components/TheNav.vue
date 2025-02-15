bbc9914c01998e1af83e1134a34264a72c3f6b71
GuiFernandes <GuiFernandesPro@gmail.com>
2025-02-15 15:32:14 +0100

    signup page complete

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,26 +1,30 @@
 <template>
-  <nav>
-    <div class="logo">
-      <h1>HTK</h1>
-    </div>
-  </nav>
+	<nav>
+		<div class="logo" @click="$router.push('/')">
+			<h1>HTK</h1>
+		</div>
+	</nav>
 </template>
 
 <style scoped>
 nav {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem 2rem;
-  background: var(--light);
-  border-bottom: 2px solid var(--primary);
-  color: var(--white);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 1rem 2rem;
+	background: var(--light);
+	border-bottom: 2px solid var(--primary);
+	color: var(--white);
+}
+
+.logo {
+  cursor: pointer;
 }
 
 .logo h1 {
-  font-size: 2rem;
-  font-family: "Montserrat", sans-serif;
-  color: var(--primary);
-  user-select: none;
+	font-size: 2.5rem;
+	font-family: "Montserrat", sans-serif;
+	color: var(--primary);
+	user-select: none;
 }
 </style>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,24 +1,77 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const email = ref();
-const password = ref();
+const email = ref('');
+const password = ref('');
+const name = ref('');
 
 const signup = () => {
-
-}
-
+  console.log(email.value, password.value);
+}	
 </script>
 
 <template>
+  <TheNav />
+
   <section>
-    <h1>Sign Up</h1>
-    <form @submit.prevent="signup">
-      <label for="email">Email</label>
-      <input type="email" id="email" v-model="email" required />
-      <label for="password">Password</label>
-      <input type="password" id="password" v-model="password" required />
-      <button>Sign Up</button>
-    </form>
+
+    <div class="modal">
+      <h1>Sign up</h1>
+      <form @submit.prevent="signup">
+        <input type="text" placeholder="Name" id="name" v-model="name" required />
+        <input type="email" placeholder="Email" id="email" v-model="email" required />
+        <input type="password" placeholder="Password" id="password" v-model="password" required />
+        <button class="primary-btn">Sign up</button>
+      </form>
+    </div>
   </section>
+
 </template>
+
+<style scoped>  
+section {
+  background-color: #FFF8EE;
+  min-height: 90vh;
+
+  display: grid;
+  place-items: center;
+}
+.modal {
+  width: 60%;
+  max-width: 600px;
+  padding: 2rem;
+
+  background-color: #FFF;
+  border: 2px solid var(--primary);
+  border-radius: 1rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  font-size: 2rem;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+
+  color: var(--dark-light);
+  margin-bottom: 1rem;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 2.5rem;
+}
+
+input {
+  padding: 1rem 1rem;
+  border: 2px solid var(--primary);
+  border-radius: 0.5rem;
+  font-size: 1.1rem;
+}
+
+button {
+  margin: 1rem 0;
+  font-size: 1.1rem;
+}
+</style>
