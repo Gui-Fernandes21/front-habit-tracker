c86cad14c3af87ffc2028ead6cbd6ec21b9c8b4c
GuiFernandes <GuiFernandesPro@gmail.com>
2025-02-22 12:46:03 +0100

    time bugfix

--- utils/timeFunctions.js
+++ utils/timeFunctions.js
@@ -1,7 +1,7 @@
 export const generateTimeArr = (range, step = 1) => {
 	const arr = [];
 	for (let i = 0; i <= range; i = i + step) {
-		const str = i.toString().length === 1 ? `0${i}` : i;
+		const str = i.toString().length === 1 ? `0${i}` : `${i}`;
 		arr.push(str);
 	}
 	return arr;
