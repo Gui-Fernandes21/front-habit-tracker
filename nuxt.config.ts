00f6d1143ec8d93d65817a10aa950cc42c79972a
GuiFernandes <GuiFernandesPro@gmail.com>
2025-03-15 16:16:50 -0400

    nuxt config updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- devtools: { enabled: false },
- css: ["~/assets/css/main.css"],
+	devtools: { enabled: false },
+	css: ["~/assets/css/main.css"],
 
- app: {
-	head: {
-		title: "Habit Tracker",
+	app: {
+		head: {
+			title: "Habit Tracker",
+		},
 	},
-},
 
- runtimeConfig: {
-	public: {
-		apiUrl: process.env.NUXT_PUBLIC_API_URL || "https://htk-api-414059395729.us-central1.run.app",
-		newsApiKey: process.env.NUXT_PUBLIC_NEWS_API_KEY
+	runtimeConfig: {
+		public: {
+			apiUrl:
+				process.env.NUXT_PUBLIC_API_URL ||
+				"https://htk-api-414059395729.us-central1.run.app",
+			newsApiKey: process.env.NUXT_PUBLIC_NEWS_API_KEY,
+		},
 	},
-},
 
- nitro: {
-	preset: "firebase",
-	firebase: {
-		gen: 2,
-		serverFunctionName: "ssr",
+	nitro: {
+		preset: "firebase",
+		firebase: {
+			gen: 2,
+			serverFunctionName: "ssr",
+		},
 	},
-},
 
- compatibilityDate: "2025-03-05"
+	compatibilityDate: "2025-03-05",
 });
